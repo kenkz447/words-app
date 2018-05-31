6b3064c2b2efab194e5e07a531f05dd2950e7dde
kenkz447 <kenkz447@gmail.com>
2018-05-31 22:28:28 +0700

    update

---
+++ src/main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './Main.styles'
+
+export class Main extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
